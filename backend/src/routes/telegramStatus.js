c6d3cf65b4a6f05ca5d274f208902916ed6f4876
Zelalem Argaw <thelalemarg@gmail.com>
2026-03-13 16:27:53 +0300

    fixes

--- backend/src/routes/telegramStatus.js
+++ backend/src/routes/telegramStatus.js
@@ -5,8 +5,7 @@ const router = express.Router();
 
 router.get("/telegram-status/:token", async (req, res) => {
   const user = await User.findOne({ connectToken: req.params.token });
-  console.log(user)
-  res.json({ connected: !!user?.username,user });
+  res.json({ connected: !!user?.telegramId, user });
 });
 
 export default router;
